0ddc3c91446c4dc767934df348bc12253897d8b4
Mike McAlister <m@mikemcalister.com>
2019-01-14 15:24:59 -0600

    Check for font_family variable.
    Closes #394202902

--- functions.php
+++ functions.php
@@ -197,14 +197,18 @@ function atomic_blocks_fonts_url() {
 		}
 	}
 
-	$query_args = array(
-		'family' => urlencode( implode( '|', $font_families ) ),
-		'subset' => urlencode( 'latin,latin-ext' ),
-	);
+	if ( !empty ( $font_families ) ) {
 
-	$fonts_url = add_query_arg( $query_args, "//fonts.googleapis.com/css" );
+		$query_args = array(
+			'family' => urlencode( implode( '|', $font_families ) ),
+			'subset' => urlencode( 'latin,latin-ext' ),
+		);
 
-	return $fonts_url;
+		$fonts_url = add_query_arg( $query_args, "//fonts.googleapis.com/css" );
+
+		return $fonts_url;
+
+	}
 }
 endif;
 
